6fa60fb5bcac838234b9806cc54a18dc3f075794
Petr Berezhkov <petr.berezhkov@noones.team>
2025-02-13 21:20:47 +0300

    first version

---
+++ src/StringProcessor.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types = 1);
+
+namespace Brzhkv\OtusPhpComposer2501;
+
+class StringProcessor
+{
+    public function getLength(string $s): int
+    {
+        return strlen($s);
+    }
+}
